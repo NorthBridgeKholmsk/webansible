1b059d64d11bac87ad9ff6ffd484284fd02e8a10
Roman <roman216rf@gmail.com>
2024-04-17 04:44:53 +0000

    Добавлено описание проекта

---
+++ public/assets/menuTurn-27e11e0136e4f439f295e7062f4b77f3768ef4724731a6e359fce56a013aa370.js
@@ -0,0 +1,33 @@
+var menuSlide = [
+    document.getElementById("logo_img"),
+    document.getElementById("btn_invent_text"),
+    document.getElementById("btn_project_text"),
+    document.getElementById("btn_run-playbook_text"),
+    document.getElementById("btn_settings_text"),
+];
+
+function menuTurn(){
+    if (menuSlide[0].style.display === "none"){
+        for (let i=0; i < menuSlide.length; i++){
+            menuSlide[i].style.removeProperty("display");
+        }
+        document.getElementById("btn-slide-menu").classList.add("bi-caret-left-fill");
+        document.getElementById("btn-slide-menu").classList.remove("bi-caret-right-fill");
+    }
+    else{
+        var openElements = document.getElementsByClassName("show");
+        if (openElements.length > 0){
+            openElements[0].previousElementSibling.children[0].click();
+        }
+        for (let i=0; i < menuSlide.length; i++){
+            menuSlide[i].style.display = "none";
+        }
+        document.getElementById("btn-slide-menu").classList.remove("bi-caret-left-fill");
+        document.getElementById("btn-slide-menu").classList.add("bi-caret-right-fill");
+    }
+}
+
+function clickMenuTurn(){
+    menuSlide[0].style.display = "none";
+    menuTurn();
+};
---
+++ public/assets/varsControl-2ef7212af55d585b413b124020c9e0e195e765eb7e1bacd7a2f6f69246b60c11.js
@@ -0,0 +1,31 @@
+function addVar(id){
+    if (id) {
+        const btn_addVar = document.getElementById("btn_addVar" + id);
+        const newVar = document.getElementById("newVar" + id).cloneNode(true);
+        newVar.id = "";
+        newVar.classList.remove("visually-hidden");
+        document.getElementById("vars" + id).insertBefore(newVar, btn_addVar);
+    }
+}
+
+function delVar(e){
+    e.parentElement.parentElement.remove();
+}
+
+function getAllVars(id){
+    const group_variables = document.getElementById("variables"+id);
+    var variables = new Map();
+    const rowsVar = document.getElementById("vars"+id).getElementsByClassName("row");
+
+    for (var i = 2; i < rowsVar.length; i++){
+        var varItem = rowsVar[i].children;
+
+        if (varItem[0].children[0].value.trim() !== "" && varItem[1].children[0].value.trim() !== ""){
+            variables.set(varItem[0].children[0].value.trim(), varItem[1].children[0].value.trim());
+        }
+    }
+    group_variables.value = ""
+    variables.forEach((value, key, map) =>{
+        group_variables.value += key + ":" + value + ",";
+    });
+};
